ef5bb2b0411598908c8fa821a335bdaf2d400808
ygor-amorim <ygoramorim3@hotmail.com>
2022-08-10 16:15:48 +0100

    break continue study

--- NodeJs/Fundamentos/3.Estruturas_de_Controle/break_continue.js
+++ NodeJs/Fundamentos/3.Estruturas_de_Controle/break_continue.js
@@ -1,6 +1,15 @@
-for (let = 1; i <= 5; 1++){
+// break
+for (let i = 1; i <= 5; i++){
+  if (i === 3) {
+    break;
+  }
   console.log(i);
 }
-for (let = 1; i <= 5; 1++){
+
+// continue
+for (i = 1; i <= 5; i++){
+  if (i === 3) {
+    continue;
+  }
   console.log(i);
 }
